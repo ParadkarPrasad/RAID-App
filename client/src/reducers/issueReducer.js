a748a0ebe1b154d71ed3522038b740cf8e6404b1
ParadkarPrasad <prasadparad37@gmail.com>
2025-10-04 18:46:36 +0100

    issue redducer

---
+++ client/src/reducers/issueReducer.js
@@ -0,0 +1,98 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import issueService from "../services/issue"
+
+
+export const fetchIssues = createAsyncThunk(
+  'issues/fetchAll',
+  async (projectId, thunkAPI) => {
+    try {
+      const issues = await issueService.getAll(projectId)
+      return issues
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message)
+    }
+  }
+)
+
+export const createIssue = createAsyncThunk(
+  'issues/createIssue',
+  async ({ projectId, newIssue }, thunkAPI) => {
+    try {
+      return await issueService.createIssue(projectId, newIssue)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message)
+    }
+  }
+)
+export const updateIssue = createAsyncThunk(
+  'issues/updateIssue',
+  async ({ issueId, projectId, updatedIssue }, thunkAPI) => {
+    try {
+      return await issueService.createIssue(issueId, projectId, updatedIssue)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message)
+    }
+  }
+)
+
+export const deleteIssue = createAsyncThunk(
+  "issues/deleteIssue",
+  async ({ projectId, issueId }, thunkAPI) => {
+    if (!projectId || !issueId) {
+      return thunkAPI.rejectWithValue("Invalid projectID or issueId")
+    }
+    try {
+      return issueService.deleteIssue(projectId, issueId)
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message)
+    }
+  }
+)
+
+const initialState = {
+  issues: [],
+  loading: false,
+  error: null
+}
+
+const issueSlice = createSlice({
+  name: "issues",
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+
+    builder
+      .addCase(fetchIssues.pending, state => {
+        state.loading = true
+        state.error = null
+      })
+
+      .addCase(fetchIssues.fulfilled, (state, action) => {
+        state.issues = action.payload
+        state.loading = false
+      })
+      .addCase(fetchIssues.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+
+      // Create
+      .addCase(createIssue.fulfilled, (state, action) => {
+        state.issues = [...state.issues, action.payload]
+      })
+
+      // Update
+      .addCase(updateIssue.fulfilled, (state, action) => {
+        const updated = action.payload
+        state.issues = state.issues.map((issue) => issue.id !== updated.id ? issue : updated)
+      })
+
+      // Delete
+      .addCase(deleteIssue.fulfilled, (state, action) => {
+        state.issues = state.issues.filter((issue) => issue.id !== action.payload.id)
+      })
+  }
+})
+
+export default issueSlice.reducer
--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@ import authReducer from "./reducers/authReducer"
 import projectReducer from "./reducers/projectReducer"
 import riskReducer from "./reducers/riskReducer"
 import assumptionReducer from "./reducers/assumptionReducer"
+import issueReducer from "./reducers/issueReducer"
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     projects: projectReducer,
     risks: riskReducer,
-    assumptions: assumptionReducer
+    assumptions: assumptionReducer,
+    issues: issueReducer,
 
   },
 });
